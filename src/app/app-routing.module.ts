3db793d8b8a6f9dbb2e5efa22bfd31ac19ddf414
aria-dev <iamshanuuknow@outlook.com>
2020-05-18 18:16:22 +0530

    this is somewhat stupid

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router'; // CLI imports router
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  }
+]; // sets up routes constant where you define your routes
+
+// configures NgModule imports and exports
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module'; // CLI imports AppRoutingModule
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { ToolbarComponent } from './toolbar/toolbar.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, ToolbarComponent ],
+  imports:      [ BrowserModule, FormsModule, AppRoutingModule],
+  declarations: [ AppComponent, HelloComponent, ToolbarComponent, HomeComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
